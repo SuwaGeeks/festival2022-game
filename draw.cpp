68d06ccc6e7f747b0c01af4eca93df9351af0efb
Apricot256 <apricot4096@gmail.com>
2022-10-25 11:46:54 +0900

    Delete draw class

--- draw.cpp
+++
@@ -1,5 +0,0 @@
-#include <Dxlib.h>
-
-void SuwaGeeks(){
-    DrawString( 0 , 0 , "SuwaGeeks" , GetColor( 0 , 255 , 0 )) ;
-}
--- draw.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef __DRAW_HPP_INCLUDED__
-#define __DRAW_HPP_INCLUDED__
-
-void SuwaGeeks();
-
-#endif
