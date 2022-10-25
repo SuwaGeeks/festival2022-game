63dd9b7d627521c0e0c50fd1ad8d28e6c49001c6
mochi <mochi.2225.it@gmail.com>
2022-10-25 11:03:00 +0900

    update: Add constructor.

--- enemy.cpp
+++ enemy.cpp
@@ -1,7 +1,10 @@
-class enemy {
+#include <Dxlib.h>
+
+class Enemy {
   private:
     int posX, posY;
 
   public:
     double act(int *x, int *y, int *dy);
+    Enemy(int x, int y, int dx, int dy);
 };
--- player.cpp
+++ player.cpp
@@ -1,7 +1,10 @@
-class player {
+#include <Dxlib.h>
+
+class Player {
   private:
     int posX, posY;
 
   public:
     double act(int *x, int *y, int *dy);
+    Player(int x, int y);
 };
