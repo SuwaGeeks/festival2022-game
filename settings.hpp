9c92d75787d43c02e36b6c31ea8dab5bc119ef68
Yudai Fujimoto <apricot4096@gmail.com>
2022-10-26 19:34:06 +0900

    Delete Utils Class

---
+++ settings.hpp
@@ -0,0 +1,10 @@
+
+
+// window関係
+#define WINDOW_HEIGHT 800
+#define WINDOW_WIDTH 600
+#define WINDOW_BIT_DEPTH 32
+#define WINDOW_MODE true
+#define WINDOW_SCALABLE false
+#define WINDOW_TITLE "f2022-game"
+#define WINDOW_SCALE 1.0
