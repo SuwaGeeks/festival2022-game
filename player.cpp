0bd329de6ae4a7f3afff44a8b874943a2c183cd8
mochi-yu <mochi.2225.it@gmail.com>
2022-10-26 23:30:45 +0900

    add: Add player class.

--- player.cpp
+++ player.cpp
@@ -1,10 +1,41 @@
 #include <Dxlib.h>
+#include <stdlib.h>
 #include "player.hpp"
+#include "graphics.hpp"
+#include "settings.hpp"
 
-Player::Player(int x, int y){
-
+// コンストラクタ
+Player::Player(double x, double y){
+  this->posX = x;
+  this->posY = y;
 }
 
-double Player::act(int *x, int *y, int *dy){
-    return 0.0;  // returnないとwarning出るので仮でつけました。
+// プレイヤーの行動
+void Player::act(double *x, double *y, double *dy){
+  // dyを初期化
+  *dy = 0;
+
+  // 左右の移動(左右どちらも押している場合には停止する)
+  if(CheckHitKey(KEY_INPUT_LEFT) || CheckHitKey(KEY_INPUT_A)){
+    this->posX -= PLAYER_DELTA_X;
+  }
+  if(CheckHitKey(KEY_INPUT_RIGHT) || CheckHitKey(KEY_INPUT_D)){
+    this->posX += PLAYER_DELTA_X;
+  }
+
+  // 玉の発射
+  if(CheckHitKey(KEY_INPUT_Z) || CheckHitKey(KEY_INPUT_SPACE)){
+    *dy = 1;
+  }
+}
+
+// ゲッタ
+void Player::getXY(int *x, int *y){
+  *x = this->posX;
+  *y = this->posY;
+}
+
+// 描画関数
+void Player::draw(){
+  DrawGraph((int)(this->posX)-(this->width)/2 ,(int)(this->posY)-(this->height)/2 ,graphics[this->graphic] , FALSE);
 }
--- player.hpp
+++ player.hpp
@@ -1,13 +1,21 @@
 #ifndef __PLAYER_HPP_INCLUDED__
 #define __PLAYER_HPP_INCLUDED__
 
+#include <string>
+
 class Player {
   private:
-    int posX, posY;
+    double posX, posY;
+    const int height = 50, width = 50;
+
+    // グラフィックのKey
+    const std::string graphic = "enemy1";
 
   public:
-    double act(int *x, int *y, int *dy);
-    Player(int x, int y);
+    void act(double *x, double *y, double *dy);
+    void getXY(int *x, int *y);
+    void draw();
+    Player(double x, double y);
 };
 
 #endif
